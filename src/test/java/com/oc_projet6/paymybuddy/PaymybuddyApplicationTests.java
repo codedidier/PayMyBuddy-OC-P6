07f8a7c64468643570099181c1816c873eedaaa8
Didier Cotrel <didier.cotrel@gmail.com>
2022-03-11 14:42:00 +0100

    initialisation du projet

---
+++ src/test/java/com/oc_projet6/paymybuddy/PaymybuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc_projet6.paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
